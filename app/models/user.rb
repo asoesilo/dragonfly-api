9e762ff41d9ace39ec499ff783e63b69fd11e2ff
Andre Soesilo <asoesilo84@gmail.com>
2014-06-23 08:26:58 +0000

    fix bugs: gender and user association; as_json representation of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_one :gender
+  belongs_to :gender
   has_many :languages, foreign_key: :user_id, class_name: :UserLanguage, dependent: :destroy
   default_value_for :is_online, false
 
@@ -9,18 +9,21 @@ class User < ActiveRecord::Base
   validates :lastname, presence: true
 
   def teaching_languages
+    # TODO:
   end
 
   def learning_languages
+    # TODO:
   end
 
-  def to_json
+  def as_json(options)
     {
+      id: id,
       email: email,
       firstname: firstname,
       lastname: lastname,
       birthday: birthday,
-      gender: gender.name,
+      gender: gender.description,
       about: about,
       is_online: is_online
     }
