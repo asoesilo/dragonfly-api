cc6f7dfd911d2d67e85f214aa83ec226df23f348
Andre Soesilo <asoesilo84@gmail.com>
2014-06-23 05:31:42 +0000

    implement part of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,36 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user, only: [:create]
+
   def create
+    @user = User.new(user_params)
+    @user.is_online = true
+    if @user.save
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
+    end
   end
 
   def details
+    render json: current_user, status: :ok
   end
 
   def update
+    if current_user.update_attributes(user_params)
+      head :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :bad_request
+    end
   end
 
   def destroy
+    if current_user.destroy
+      send_offline_notice
+      log_out
+      head :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :bad_request
+    end
   end
 
   def matches
@@ -21,5 +43,13 @@ class UsersController < ApplicationController
   end
 
   def offline
+  end
+
+  private
+  def user_params
+    params(:user).permit(:email, :password, :firstname, :lastname, :birthday, :gender, :about)
+  end
+
+  def send_offline_notice
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_one :gender
-  has_many :languages, foreign_key: :user_id, class_name: :UserLanguage
+  has_many :languages, foreign_key: :user_id, class_name: :UserLanguage, dependent: :destroy
   default_value_for :is_online, false
 
   validates :email, presence: true, uniqueness: true, email: true
   validates :firstname, presence: true
   validates :lastname, presence: true
+
+  def teaching_languages
+  end
+
+  def learning_languages
+  end
+
+  def to_json
+    {
+      email: email,
+      firstname: firstname,
+      lastname: lastname,
+      birthday: birthday,
+      gender: gender.name,
+      about: about,
+      is_online: is_online
+    }
+  end
 end
